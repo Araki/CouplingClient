d9e1d5bef8d22466e3eb3a651b470ba7ba30cb1b
Kamei <777999666kamei@gmail.com>
2013-03-30 21:09:26 +0900

    PFTalkDataModel追加

---
+++ Coupling/Classes/PFTalkDataModel.h
@@ -0,0 +1,13 @@
+//
+//  PFTalkDataModel.h
+//  Coupling
+//
+//  Created by Ryo Kamei on 13/03/30.
+//  Copyright (c) 2013年 tsuchimoto. All rights reserved.
+//
+
+#import "PFDataModel.h"
+
+@interface PFTalkDataModel : PFDataModel
+
+@end
---
+++ Coupling/Classes/PFTalkDataModel.m
@@ -0,0 +1,13 @@
+//
+//  PFTalkDataModel.m
+//  Coupling
+//
+//  Created by Ryo Kamei on 13/03/30.
+//  Copyright (c) 2013年 tsuchimoto. All rights reserved.
+//
+
+#import "PFTalkDataModel.h"
+
+@implementation PFTalkDataModel
+
+@end
