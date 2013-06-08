f6dcc6ef1b3c49dddc8df20d706941309ed8b560
uniom <hi.tsutti@gmail.com>
2013-06-08 17:09:46 +0900

    ログイン処理実装（現状はサインアップ処理と同じ）

--- Coupling/TitleViewController.m
+++ Coupling/TitleViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "TitleViewController.h"
+#import "PFNotificationsName.h"
+#import "FBManager.h"
+#import "PFHTTPConnector.h"
+#import "PFMiscUtil.h"
 #import "PFViewDeckController.h"
 
 @interface TitleViewController ()
@@ -28,6 +32,10 @@
 {
     [super viewDidLoad];
     self.view.backgroundColor = kPFCommonBackGroundColor;
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(sessionStateChanged:)
+                                                 name:kPFNotificationFBSessionStateChanged
+                                               object:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,6 +48,42 @@
 {
     [super viewWillAppear:animated];
     self.navigationController.navigationBarHidden = YES;
+}
+
+#pragma mark original method
+
+- (IBAction)loginFacebook:(id)sender {
+    [[FBManager sharedObject] openSessionWithAllowLoginUI:YES];
+}
+
+- (void)sessionStateChanged:(NSNotification *)notification
+{
+    FBSession *fbSession = (FBSession *)notification.object;
+    
+    if (fbSession.state == FBSessionStateOpen) {
+        NSString *uuid = [PFMiscUtil readUUID];
+        if (uuid == nil) {
+            uuid = [PFMiscUtil createUUID];
+        }
+        NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+                                fbSession.accessToken, @"access_token", uuid, @"device_token", nil];
+        [PFHTTPConnector postWithCommand:kPFCommandSessionsCreate params:params onSuccess:^(PFHTTPResponse *response) {
+            NSLog(@"@@@@@ postWithCommand onSuccess");
+            NSString *session = [[response jsonDictionary] valueForKey:@"session"];
+            [PFUser currentUser].sessionId = session;
+            [[PFUser currentUser] saveToCacheAsCurrentUser];
+            
+            dispatch_queue_t mainQueue = dispatch_get_main_queue();
+            dispatch_async(mainQueue, ^{
+                UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Storyboard" bundle:nil];
+                PFViewDeckController *view = [storyboard instantiateViewControllerWithIdentifier:@"PFViewDeckController"];
+                [self.navigationController pushViewController:view animated:YES];
+            });
+            
+        } onFailure:^(NSError *error) {
+            NSLog(@"@@@@@ connection Error: %@", error);
+        }];
+    }
 }
 
 @end
