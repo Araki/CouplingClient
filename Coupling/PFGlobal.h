baefba70fc4bea73c47bf7b7ba4f4100d4b8529f
uniom <hi.tsutti@gmail.com>
2013-08-13 19:52:39 +0900

    コメントアウト削除

--- Coupling/PFGlobal.h
+++ Coupling/PFGlobal.h
@@ -9,9 +9,7 @@
 #ifndef Coupling_PFGlobal_h
 #define Coupling_PFGlobal_h
 
-//#define kPFEndpointBase							@"https://api.pairful.net/api"
 #define kPFEndpointBase							@"http://api-staging1.pairful.net/api"
-//#define kPFEndpointBase							@"http://0.0.0.0:3000/api"
 
 #endif
 
